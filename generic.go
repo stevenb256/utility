4f5778846f4f2921959e81de779a4a9371963ca7
Steve <sbailey@microsoft.com>
2020-01-06 07:43:04 -0800

    add min max functins

--- generic.go
+++ generic.go
@@ -10,3 +10,19 @@ func md5Bytes(buf []byte) string {
 	h := md5.Sum(buf)
 	return base64.StdEncoding.EncodeToString(h[:])
 }
+
+// MinUint32 min of two uint32
+func MinUint32(x, y uint32) uint32 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// MaxUint32 Max of two uint32
+func MaxUint32(x, y uint32) uint32 {
+	if x > y {
+		return x
+	}
+	return y
+}
