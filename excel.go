358f61d18c7f74cbb9d0ee3c5eb4485c8a351be7
Steve Bailey <sbailey@microsoft.com>
2021-02-15 14:12:17 -0800

    new excel lib

--- excel.go
+++ excel.go
@@ -47,7 +47,10 @@ func OpenExcel(reader io.Reader, columns []string) (*Excel, error) {
 	excel.sheet = file.Sheets[0]
 
 	// get row
-	excel.row = excel.sheet.Row(0)
+	excel.row, err = excel.sheet.Row(0)
+	if l.Check(err) {
+		return nil, err
+	}
 
 	// if no rows
 	if nil == excel.row {
@@ -59,8 +62,8 @@ func OpenExcel(reader io.Reader, columns []string) (*Excel, error) {
 
 	// find columns
 	for _, name := range columns {
-		for i := 0; i < len(excel.row.Cells); i++ {
-			if name == strings.ToLower(excel.row.Cells[i].Value) {
+		for i := 0; i < excel.row.Sheet.MaxCol; i++ {
+			if name == strings.ToLower(excel.row.GetCell(i).Value) {
 				excel.columns[name] = i
 				break
 			}
@@ -79,10 +82,14 @@ func OpenExcel(reader io.Reader, columns []string) (*Excel, error) {
 // IsDone returns true if no more rows
 func (e *Excel) IsDone() bool {
 	if nil != e.row {
+		var err error
 		e.i++
-		e.row = e.sheet.Row(e.i)
+		e.row, err = e.sheet.Row(e.i)
+		if l.Check(err) {
+			e.row = nil
+		}
 	}
-	if nil == e.row || 0 == len(e.row.Cells) {
+	if nil == e.row || 0 == e.row.Sheet.MaxCol {
 		return true
 	}
 	return false
@@ -94,7 +101,7 @@ func (e *Excel) getIndex(name string) int {
 	if false == found {
 		return -1
 	}
-	if nil == e.row || i >= len(e.row.Cells) {
+	if nil == e.row || i >= e.row.Sheet.MaxCol {
 		return -1
 	}
 	return i
@@ -106,7 +113,7 @@ func (e *Excel) String(name string) string {
 	if -1 == i {
 		return ""
 	}
-	return e.row.Cells[i].Value
+	return e.row.GetCell(i).Value
 }
 
 // Int gets current row/column as string
@@ -115,7 +122,7 @@ func (e *Excel) Int(name string) int {
 	if -1 == i {
 		return 0
 	}
-	return Atoi(e.row.Cells[i].Value)
+	return Atoi(e.row.GetCell(i).Value)
 }
 
 // Date gets current row column as date
@@ -125,5 +132,5 @@ func (e *Excel) Date(name string) (time.Time, error) {
 		var t time.Time
 		return t, nil
 	}
-	return e.row.Cells[i].GetTime(false)
+	return e.row.GetCell(i).GetTime(false)
 }
